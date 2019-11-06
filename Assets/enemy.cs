5046e7c86bcfc8f721f86cb69f11b43a9432900a
kimy <yuutom15@gmail.com>
2019-11-06 11:09:37 +0900

    ff

---
+++ Assets/enemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    public float speed = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+// Update is called once per frame
+void Update()
+{
+        transform.Rotate( 1f, 1f, 1f );
+    transform.Translate( 0, 0, -speed * Time.deltaTime );
+        if (transform.position.z > 30) {
+         //   Destroy( GameObject );
+        }
+
+        }
+   // private void OnColisionEnter(Colision colision) {
+     //   if (colision.gameObject.tag == "enemy") {
+       //     Destroy(enemy);
+        //}
+    //}
+}
+    
---
+++ Assets/enemybox.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemybox: MonoBehaviour
+{
+    public GameObject prefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine( CreateEnemy() );
+    }
+
+
+    IEnumerator CreateEnemy()
+    {
+        while (true) {
+            yield return new WaitForSeconds( 1.0f );
+            Instantiate(
+                prefab,
+                new Vector3( Random.Range( -10, 11 ), 1f, 15f ),
+                Quaternion.identity
+                );
+        }
+    }
+}
